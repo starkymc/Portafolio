c00aa7cd45c079b8706f43de4fe72a026cf1090e
Starky <60411417+starkymc@users.noreply.github.com>
2022-12-12 14:07:09 -0500

    Update forms.py

--- portafoliopersonal/forms.py
+++ portafoliopersonal/forms.py
@@ -1,12 +1,9 @@
 from django import forms
 from portafoliopersonal.models import Portafolio
 
-class LoginForm(forms.Form):
-    name = forms.CharField(max_length=50)
-    password = forms.CharField(max_length=50)
 
 
 class PortafioForm(forms.ModelForm):
     class Meta:
         model = Portafolio
-        fields = ('__all__')
+        fields = ['foto','project_name', 'description', 'tags','github']

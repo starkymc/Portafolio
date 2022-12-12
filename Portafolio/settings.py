d4739f4c56fe2269c2c3d96173f3dd376d8e230f
Starky <60411417+starkymc@users.noreply.github.com>
2022-12-11 23:29:03 -0500

    Update settings.py

--- Portafolio/settings.py
+++ Portafolio/settings.py
@@ -134,7 +134,8 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
+                          
+CSRF_TRUSTED_ORIGINS = [ 'https://portafolio-production-66d6.up.railway.app']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field

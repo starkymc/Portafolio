2f6af687b50051d257a7cde559a0fbdf199c3a39
Starky <60411417+starkymc@users.noreply.github.com>
2022-12-14 12:02:17 -0500

    Update admin.py

--- portafoliopersonal/admin.py
+++ portafoliopersonal/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Portafolio, IpAddress
 
 # Register your models here.
+class PortafolioAdmin(admin.ModelAdmin):
+    admin.site.register(Portafolio)
+    admin.site.register (IpAddress)

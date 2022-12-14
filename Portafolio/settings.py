b4d62aa680128b9e9524a8b55eefcccf242f9483
Starky <60411417+starkymc@users.noreply.github.com>
2022-12-14 12:00:21 -0500

    Update settings.py

--- Portafolio/settings.py
+++ Portafolio/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'portafoliopersonal.ip.saveip_address',
 ]
 
 ROOT_URLCONF = 'Portafolio.urls'
